6f664772972b146b7f44f78f74d2fc23cbd507cd
Stevie <stevie.de.simone1@gmail.com>
2021-02-13 13:39:05 +0100

    Ex 2, first draft

---
+++ Filippo/Es1b.java
@@ -0,0 +1,30 @@
+/* Es1a: Si scriva un programma con i seguenti metodi: 
+bisestile: dato un anno ritorna se è bisestile o no. Cercate su wikipedia la regola se non la conoscete.
+params: anno:int,
+return: boolean 
+
+inverti: dato un array, restituisce l'array al contrario. Es: {1,2,3,4,5,6} ritorna: {6,5,4,3,2,1}
+params: array: int[]
+return: int[] */ 
+
+
+import java.util.Scanner;
+
+public class Es1b {
+    public static void main (String args[]) {
+        int [] arr = {1,2,3,4,5,6};
+        int [] reversed = arr; 
+        for (int i=0; i<reversed.length; i++) {
+            System.out.println(reversed[i]);
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
