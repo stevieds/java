de1906a6e3044ab9bf1d8decd027c1bc92184adf
Stevie <stevie.de.simone1@gmail.com>
2021-02-13 12:48:56 +0100

    Amended condition in method leapy

--- Filippo/Es1a.java
+++ Filippo/Es1a.java
@@ -8,28 +8,21 @@ public class Es1a {
         System.out.println("Insert a year");
         int year = scan.nextInt();
         boolean leap=leapy(year);
-        
-
-
 
         System.out.println(year+leapText(leap));
         
-
-
-        
-
     }
 
     public static boolean leapy (int year) {
         boolean isleap;
-        if (year % 4 != 0 || year % 400 != 0) {
-            isleap = false;
+        if (year % 100 !=0 && year % 4 == 0) {
+            isleap=true;
         }
-        else if (year % 100 != 0) {
-            isleap = true;             
+        else if (year % 100 == 0 && year % 400 == 0) {
+            isleap = true;
         }
         else {
-            isleap = true;
+            isleap = false;
         }
         return isleap;
     }
